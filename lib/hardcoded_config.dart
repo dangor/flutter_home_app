6b9e5ed9cca1d4e9b2d5b07dcabf2aa59d5ad578
Brian Dang <briancdang@gmail.com>
2019-02-02 19:12:59 -0800

    Add dishwasher

--- lib/hardcoded_config.dart
+++ lib/hardcoded_config.dart
@@ -25,6 +25,8 @@ class HardcodedConfig {
     ItemConfig("launderClothes", "Laundry - Clothes", "TODO", Duration(days: 4)),
     ItemConfig("launderLinens", "Laundry - Linens", "TODO", Duration(days: 14)),
     ItemConfig("vacuum3f", "Vacuum 3rd floor", "TODO", Duration(days: 7)),
+    ItemConfig("rundishwasher", "Run dishwasher", "TODO", Duration(days: 4)),
+    ItemConfig("emptydishwasher", "Empty dishwasher", "TODO", Duration(days: 4)),
   ]);
 
   static List<UserConfig> userConfig = List.unmodifiable([
@@ -34,7 +36,7 @@ class HardcodedConfig {
 
   static List<ItemPageConfig> pageConfig = List.unmodifiable([
     ItemPageConfig("page0", "First floor", ["dogpee", "dogfood", "glucosamine", "dogwalk", "dogteeth", "dognails", "guido", "vacuum1f"].toSet()),
-    ItemPageConfig("page1", "Kitchen", ["probiotic", "plants", "jade", "rinseaid", "sponge"].toSet()),
+    ItemPageConfig("page1", "Kitchen", ["rundishwasher", "emptydishwasher", "rinseaid", "probiotic", "plants", "jade", "sponge"].toSet()),
     ItemPageConfig("page2", "Third floor", ["litterbox", "catfood", "vacuum3f", "launderClothes", "launderLinens"].toSet()),
   ]);
 }
